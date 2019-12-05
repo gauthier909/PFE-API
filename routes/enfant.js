1d6ecd06c13e7caf8d26916273b0138ca14f8f1c
CuratoloG <34574535+CuratoloG@users.noreply.github.com>
2019-12-05 18:13:57 +0100

    find all enfants

--- routes/enfant.js
+++ routes/enfant.js
@@ -3,16 +3,19 @@ const router = express.Router()
 const db = require('../utils/db')
 
 // /api/enfant/
+// Find all enfants
 router.get('/', (req, res) => {
     db.mongo
     .collection("enfants")
-    .findOne()
-    .then(enfant => {
-      res.json(enfant);
-    })
-
-
+    .find()
+    .toArray()
+    .then(enfants => {
+      res.json(enfants);
+    }).catch((err) => {
+		res.status(500).send(err)
+	});
 })
+
 
 // /api/enfant/jeu
 router.get('/jeu', () => {
