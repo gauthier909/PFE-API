3e259607a93e2472d4c8d66316c77d5cbfb204b7
unknown <gauthier.rogerfrance@student.vinci.be>
2019-12-07 20:55:30 +0100

    crud personne

--- routes/personne.js
+++ routes/personne.js
@@ -29,4 +29,56 @@ router.post('/', (req, res) => {
     });
   });
 
+  //get personne by id
+  router.get('/:id', (req, res) => {
+    //console.log('test get id')
+    db.mongo
+    .collection("personnes")
+    .findOne({_id: new db.ObjectID(req.params.id)})
+    .then(personne => {
+      res.json(personne);
+      console.log(personne)
+    }).catch((err) => {
+    res.status(500).send(err)
+  });
+  })
+
+
+  // Update personne by ID
+router.put('/:id', function(req, res) {
+    console.log("message update reçu")
+    
+    console.log(req.body._id)
+    delete req.body._id;
+      db.mongo
+    .collection("personnes")
+    .findOneAndUpdate({_id: new db.ObjectID(req.params.id)}, {$set: {"nom":req.body.nom,  "prenom": req.body.prenom, "email":req.body.email,"profession":req.body.profession,"role":req.body.role,"telephone":req.body.telephone }}, {returnOriginal: false})
+    .then((result) => {
+          if (result.value) {
+              res.json(result.value)
+          } else {
+              res.status(404).send()
+          }
+      }).catch((err) => {
+          res.status(500).send(err)
+      });
+  });
+  
+  // Delete a person by ID
+  router.delete('/:id', function(req, res) {
+    console.log("message delete reçu")
+    console.log(req.body._id)
+      db.mongo
+    .collection("personnes")
+    .findOneAndDelete({_id: new db.ObjectID(req.params.id)})
+    .then((result) => {
+          if (result.value) {
+              res.json(result.value)
+          } else {
+              res.status(404).send()
+          }
+      }).catch((err) => {
+          res.status(500).send(err)
+      });
+  });
 module.exports = router
