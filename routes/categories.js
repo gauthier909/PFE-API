258517fdf1eb3a6b271e6ce46829be10a87e91ba
unknown <gauthier.rogerfrance@student.vinci.be>
2019-12-08 21:08:19 +0100

    refactor dbImage + categories js

--- routes/categories.js
+++ routes/categories.js
@@ -6,7 +6,7 @@ const db = require('../modules/db')
 // /api/enfant/
 // Find all catégories
 router.get('/', (req, res) => {
-    console.log("message get all Catégories reçu")
+    //console.log("message get all Catégories reçu")
       db.mongo
       .collection("liste-de-données")
       .findOne({"type": "catégorie"})
@@ -16,5 +16,22 @@ router.get('/', (req, res) => {
           res.status(500).send(err)
       });
   })
+
+  
+
+  router.get('/cheminImage/:categorie', (req, res) => {
+    console.log("chemin image from API")
+    //console.log(req.params.categorie)
+      db.mongo
+      .collection("images")
+      .findOne({categorie:req.params.categorie})
+      .then(image => {
+        console.log(image)
+        res.json(image.nom);
+      }).catch((err) => {
+          res.status(500).send(err)
+      });
+  })
+
   
 module.exports = router
