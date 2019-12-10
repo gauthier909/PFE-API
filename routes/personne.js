2f4515b6bb870a31c651b35d24ea630d5005c4a2
Benjamin DE BOSSCHER <benjamin.debosscher@student.vinci.be>
2019-12-10 18:40:12 +0100

    crypt ng5-auth

--- routes/personne.js
+++ routes/personne.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../modules/db')
-var crypt = require('password-hash')
+const _ =  require('lodash')
+var crypt = require('../middlewares/crypt')
 //find all personnes
 router.get('/', (req,res) => {
     console.log('message personne')
@@ -20,7 +21,7 @@ router.get('/', (req,res) => {
 router.post('/', (req, res) => {
     console.log("debut insertion personne")
     var passwordHash = req.body.password;
-    req.body.password=crypt.generate(passwordHash);
+    req.body.password=crypt.cryptPassword(passwordHash);
     db.mongo.collection("personnes").insertOne(req.body).then((result) =>{
       req.body._id = result.insertedId
       res.json(req.body)
